b095ba07d15907e4722ef71e92cae93d8f002482
SEOYOUNG BAEK <baeksy6067@gmail.com>
2025-06-13 00:25:27 +0900

    ✨ Feat: Detail 페이지에서 파라미터 id 값이랑 일치하는 포켓몬 데이터 가져오도록 수정 (#9)

--- src/pages/Detail.jsx
+++ src/pages/Detail.jsx
@@ -1,7 +1,13 @@
+import { useSelector } from "react-redux";
 import { useParams } from "react-router-dom";
 
 const Detail = () => {
   const { id } = useParams();
+  const pokemon = useSelector((state) =>
+    state.pokemon.data.find((p) => p.id === Number(id))
+  );
+  console.log(pokemon);
+
   return <div>Detail Page : {id}</div>;
 };
 
