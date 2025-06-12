6cefde07f7db99c78255807c62c5e40b9ece798d
SEOYOUNG BAEK <baeksy6067@gmail.com>
2025-06-12 18:38:03 +0900

    ✨ Feat: Home에 포켓몬 데이터를 가져오고 Card에 props로 연결 (#7)

--- src/components/Card.jsx
+++ src/components/Card.jsx
@@ -1,16 +1,14 @@
-const Card = () => {
+const Card = ({ id, color, name, sprite }) => {
   return (
     <div className="w-50 ml-30 py-4 flex flex-col items-center rounded-xl shadow-[2px_2px_0_2px_#000000]">
-      <img
-        src="https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/1.png"
-        alt="image"
-        width="150"
-      />
+      <img src={sprite} width="150" />
       <div className="w-fit h-fit border border-solid border-gray-300 flex justify-center items-center rounded-xl">
-        <span className="px-3 py-0.5 bg-green-500 font-semibold rounded-xl">
-          001
+        <span
+          className="px-3 py-0.5 font-semibold rounded-xl"
+          style={{ backgroundColor: `${color}` }}>
+          {id}
         </span>
-        <span className="px-3">이상해씨</span>
+        <span className="px-3">{name}</span>
       </div>
     </div>
   );
--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,10 +1,29 @@
 import Card from "../components/Card";
+import { useEffect } from "react";
+import { useDispatch, useSelector } from "react-redux";
+import { fetchMultiplePokemonById } from "../RTK/thunk";
 
 const Home = () => {
+  const dispatch = useDispatch();
+  const pokemonData = useSelector((state) => state.pokemon.data);
+  console.log(pokemonData);
+
+  useEffect(() => {
+    dispatch(fetchMultiplePokemonById(151));
+  }, [dispatch]);
+
   return (
     <div>
-      Home Page
-      <Card />
+      {pokemonData.length > 0 &&
+        pokemonData.map((el) => (
+          <Card
+            key={el.id}
+            id={`${el.id}`.padStart(3, "0")}
+            color={el.color}
+            name={el.name}
+            sprite={el.sprites.front_default}
+          />
+        ))}
     </div>
   );
 };
