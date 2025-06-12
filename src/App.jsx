ec4069b9fd0561d429a1f381f9c4f0b103229aa9
SEOYOUNG BAEK <baeksy6067@gmail.com>
2025-06-12 13:22:54 +0900

    ✅ Test: PokeApi 연결 테스트 (#1)

--- src/App.jsx
+++ src/App.jsx
@@ -1,4 +1,16 @@
+import { useEffect } from "react";
+
 function App() {
+  // pokeApi 연결 확인
+  useEffect(() => {
+    const fetchTest = async () => {
+      const response = await fetch("https://pokeapi.co/api/v2/pokemon/1/");
+      const data = await response.json();
+      console.log(data);
+    };
+    fetchTest();
+  }, []);
+
   return (
     <>
       <div>App</div>
