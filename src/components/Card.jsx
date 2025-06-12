49280f8c89fa2112c1ebb134a9d2241a79e0c7e7
SEOYOUNG BAEK <baeksy6067@gmail.com>
2025-06-12 17:56:57 +0900

    💄 Design: Card 컴포넌트 구조 추가 및 스타일 지정 (#7)

--- src/components/Card.jsx
+++ src/components/Card.jsx
@@ -1,5 +1,19 @@
 const Card = () => {
-  return <div>Card</div>;
+  return (
+    <div className="w-50 ml-30 py-4 flex flex-col items-center rounded-xl shadow-[2px_2px_0_2px_#000000]">
+      <img
+        src="https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/1.png"
+        alt="image"
+        width="150"
+      />
+      <div className="w-fit h-fit border border-solid border-gray-300 flex justify-center items-center rounded-xl">
+        <span className="px-3 py-0.5 bg-green-500 font-semibold rounded-xl">
+          001
+        </span>
+        <span className="px-3">이상해씨</span>
+      </div>
+    </div>
+  );
 };
 
 export default Card;
