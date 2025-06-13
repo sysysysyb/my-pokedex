b15a141351fc97b6d00d39e2b5b14653c0ee7ba2
SEOYOUNG BAEK <baeksy6067@gmail.com>
2025-06-13 17:53:42 +0900

    💄 Design: Card 스타일 수정
    Card hover 시 좋아요 버튼도 같이 크기가 커지도록 수정

--- src/components/Card.jsx
+++ src/components/Card.jsx
@@ -18,8 +18,8 @@ const Card = ({ id, color, name, sprite }) => {
   };
 
   return (
-    <Link to={`/detail/${id}`} className="relative">
-      <div className="w-50 py-4 relative bg-white flex flex-col items-center shrink-0 rounded-xl shadow-[2px_2px_0_2px_#000000] cursor-pointer duration-300 ease-out hover:scale-110">
+    <Link to={`/detail/${id}`}>
+      <div className="relative w-50 py-4 relative bg-white flex flex-col items-center shrink-0 rounded-xl shadow-[2px_2px_0_2px_#000000] cursor-pointer duration-300 ease-out hover:scale-110">
         <img src={sprite} width="150" />
         <div className="w-fit h-fit border border-solid border-gray-300 flex justify-center items-center rounded-xl">
           <span
@@ -29,17 +29,17 @@ const Card = ({ id, color, name, sprite }) => {
           </span>
           <span className="px-3">{name}</span>
         </div>
+        <button
+          type="button"
+          className="w-fit h-fit p-2 absolute top-1 right-1 cursor-pointer"
+          onClick={(e) => handleFavorite(e, id)}>
+          {favoritesList.includes(id) ? (
+            <HeartFillIcon fill="#fff" className="w-8 h-8 fill-rose-500" />
+          ) : (
+            <HeartIcon className="w-8 h-8 fill-gray-300" />
+          )}
+        </button>
       </div>
-      <button
-        type="button"
-        className="w-fit h-fit p-2 absolute top-1 right-1 cursor-pointer"
-        onClick={(e) => handleFavorite(e, id)}>
-        {favoritesList.includes(id) ? (
-          <HeartFillIcon fill="#fff" className="w-8 h-8 fill-rose-500" />
-        ) : (
-          <HeartIcon className="w-8 h-8 fill-gray-300" />
-        )}
-      </button>
     </Link>
   );
 };
