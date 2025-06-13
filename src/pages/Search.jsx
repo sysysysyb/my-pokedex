7ecf1bef4f77a9a52aff309a88d58e28415a09d3
SEOYOUNG BAEK <baeksy6067@gmail.com>
2025-06-13 12:59:16 +0900

    ✨ Feat: Search에서 쿼리 파라미터에 해당하는 이름을 가진 포켓몬 정보 가져오도록 수정 (#12)

--- src/pages/Search.jsx
+++ src/pages/Search.jsx
@@ -1,4 +1,16 @@
+import { getRegExp } from "korean-regexp";
+import { useSearchParams } from "react-router-dom";
+import { selectPokemonByRegExp } from "../RTK/selector";
+import { useSelector } from "react-redux";
+
 const Search = () => {
+  const [searchParams] = useSearchParams();
+  const param = searchParams.get("pokemon");
+  const reg = getRegExp(param);
+
+  const pokemon = useSelector(selectPokemonByRegExp(reg));
+  console.log(pokemon);
+
   return <div>Search Page</div>;
 };
 
