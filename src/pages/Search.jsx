e9f6b1486b44d045001a103fa4f2e51ce8df66e4
SEOYOUNG BAEK <baeksy6067@gmail.com>
2025-06-13 13:08:46 +0900

    ✨ Feat: 검색어로 필터링된 포켓몬 정보 Search에 렌더링 (#12)

--- src/pages/Search.jsx
+++ src/pages/Search.jsx
@@ -2,16 +2,30 @@ import { getRegExp } from "korean-regexp";
 import { useSearchParams } from "react-router-dom";
 import { selectPokemonByRegExp } from "../RTK/selector";
 import { useSelector } from "react-redux";
+import Card from "../components/Card";
 
 const Search = () => {
   const [searchParams] = useSearchParams();
   const param = searchParams.get("pokemon");
   const reg = getRegExp(param);
 
-  const pokemon = useSelector(selectPokemonByRegExp(reg));
-  console.log(pokemon);
+  const filteredPokemonData = useSelector(selectPokemonByRegExp(reg));
+  console.log(filteredPokemonData);
 
-  return <div>Search Page</div>;
+  return (
+    <section className="px-16 py-8 flex flex-wrap justify-center gap-12 bg-[#747474]">
+      {filteredPokemonData.length > 0 &&
+        filteredPokemonData.map((el) => (
+          <Card
+            key={el.id}
+            id={`${el.id}`.padStart(3, "0")}
+            color={el.color}
+            name={el.name}
+            sprite={el.sprites.front_default}
+          />
+        ))}
+    </section>
+  );
 };
 
 export default Search;
