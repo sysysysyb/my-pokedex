deffb8d87a14cd99e592898ed5e88196375c4c2c
SEOYOUNG BAEK <baeksy6067@gmail.com>
2025-06-24 15:01:55 +0900

    ♻️ Refactor: 화면 사이즈 별 Detail 스타일 수정

--- src/components/Detail.jsx
+++ src/components/Detail.jsx
@@ -4,7 +4,7 @@ import HeartIcon from "../images/heart.svg?react";
 import HeartFillIcon from "../images/heart_fill.svg?react";
 import { add, remove } from "../RTK/slice";
 import { useEffect, useState } from "react";
-import typeColorList from "../constants/typeColorList";
+import POKEMON_TYPE_LIST from "../constants/POKEMON_TYPE_LIST";
 import { useMediaQuery } from "react-responsive";
 import { useLocation, useNavigate } from "react-router-dom";
 
@@ -50,45 +50,45 @@ const Detail = () => {
     );
 
   return (
-    <aside className="pb-20 h-full w-full absolute z-1 top-1/2 left-1/2 transform -translate-x-1/2 -translate-y-1/2 bg-white">
-      <div className="w-full pl-4 py-2 mb-4 bg-red-500 text-xl text-white border-b-3 border-solid border-black shadow-[0px_5px_5px_1px_#00000015]">
+    <aside className="lg:pt-0 lg:pb-20 w-full h-full sm:absolute z-1 sm:top-1/2 sm:left-1/2 sm:transform sm:-translate-x-1/2 sm:-translate-y-1/2 bg-white">
+      <div className="hidden lg:block w-full pl-4 py-2 mb-4 bg-red-500 text-xl text-white border-b-3 border-solid border-black shadow-[0px_5px_5px_1px_#00000015]">
         설명
       </div>
-      <div className="h-full flex items-center px-5">
-        <div className="flex-2">
+      <div className="h-full flex flex-col justify-center sm:flex-row items-center px-5">
+        <div className="flex justify-center items-center sm:block">
           <div
             style={{ display: showShiny ? "none" : "inline-grid" }}
             className="perspective-midrange transform-3d rotate-y-0 duration-300 hover:perspective-midrange hover:rotate-y-180">
             <img
-              className="backface-hidden row-start-1 col-start-1 row-end-1 col-end-1"
+              className="p-2 backface-hidden row-start-1 col-start-1 row-end-1 col-end-1"
               src={pokemonData.sprites.front_default}
               alt="default"
-              width="500"
+              width="250"
             />
             <img
-              className="rotate-y-180 backface-hidden row-start-1 col-start-1 row-end-1 col-end-1"
+              className="p-2 rotate-y-180 backface-hidden row-start-1 col-start-1 row-end-1 col-end-1"
               src={pokemonData.sprites.back_default}
               alt="default"
-              width="500"
+              width="250"
             />
           </div>
           <div
             style={{ display: showShiny ? "inline-grid" : "none" }}
             className="perspective-midrange transform-3d rotate-y-0 duration-300 hover:perspective-midrange hover:rotate-y-180">
             <img
-              className="backface-hidden row-start-1 col-start-1 row-end-1 col-end-1"
+              className="p-2 backface-hidden row-start-1 col-start-1 row-end-1 col-end-1"
               src={pokemonData.sprites.front_shiny}
               alt="shiny"
-              width="500"
+              width="250"
             />
             <img
-              className="rotate-y-180 backface-hidden row-start-1 col-start-1 row-end-1 col-end-1"
+              className="p-2 rotate-y-180 backface-hidden row-start-1 col-start-1 row-end-1 col-end-1"
               src={pokemonData.sprites.back_shiny}
               alt="shiny"
-              width="500"
+              width="250"
             />
           </div>
-          <div className="flex justify-center gap-2">
+          <div className="flex flex-col sm:flex-row justify-center gap-2">
             <button
               onClick={() => setShowShiny(false)}
               className="px-2 py-1 text-gray-500 border-3 border-solid border-gray-300 rounded-lg cursor-pointer hover:bg-gray-300 hover:text-white">
@@ -101,7 +101,7 @@ const Detail = () => {
             </button>
           </div>
         </div>
-        <div className="flex-3 flex flex-col gap-10">
+        <div className="sm:basis-3/5 flex flex-col gap-5 lg:gap-10">
           <div className="shadow-[5px_5px_5px_1px_#00000015] w-full border border-solid border-gray-300 rounded-lg">
             <div className="w-full pl-5 bg-red-400 flex items-center gap-3 text-white text-lg rounded-t-lg">
               <div className="flex items-center gap-1">
@@ -137,8 +137,8 @@ const Detail = () => {
                   key={idx}
                   className="p-1 flex-1 rounded-lg flex justify-center items-center"
                   style={{
-                    boxShadow: `5px 5px 5px 1px ${typeColorList[type]}50`,
-                    background: typeColorList[type],
+                    boxShadow: `5px 5px 5px 1px ${POKEMON_TYPE_LIST[type]}50`,
+                    background: POKEMON_TYPE_LIST[type],
                   }}>
                   {type}
                 </div>
@@ -151,7 +151,7 @@ const Detail = () => {
               <div>몸무게 : {pokemonData.weight / 10}kg</div>
             </div>
           </div>
-          <div className="rounded-lg p-2 border-x-10 border-y-2 border-solid border-red-400 shadow-[5px_5px_5px_1px_#00000015]">
+          <div className="rounded-lg mb-2 p-2 border-x-10 border-y-2 border-solid border-red-400 shadow-[5px_5px_5px_1px_#00000015]">
             {pokemonData.desc}
           </div>
         </div>
